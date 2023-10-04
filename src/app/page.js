6d828e6818f26be27369cdc6af88d58c3279a337
mahbub.mohsin <mollahmahbub81@gmail.com>
2023-10-04 11:35:53 +0600

    update 2

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
-
+ 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
